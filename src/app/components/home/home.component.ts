a3d67d38f649449b52a47c794687c27e7d360b1a
hivin <pramukaluthanthri@gmail.com>
2023-10-02 16:58:52 +0530

    added homecomponent.html

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../../services/product.service";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  productList:any[]=[];
+  constructor(private productServie:ProductService) {
+  }
+  ngOnInit() {
+    debugger;
+    this.loadAllProducts()
+  }
+
+  loadAllProducts(){
+    debugger;
+    this.productServie.getAllProducts().subscribe((result:any)=>{
+      this.productList = result.data; //we decide what to fetch by looking at what the api call returns us.
+      //here the result is an object, and there's an attribute called data inside that object.
+      //That attribute contains the list of products.
+    })
+  }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllProducts():Observable<any[]>{
+    debugger;
+    return this.http.get<any[]>("https://onlinetestapi.gerasim.in/api/Ecomm/GetAllProducts");
+  }
 }
